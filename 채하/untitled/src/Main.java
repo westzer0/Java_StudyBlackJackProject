fe8eb94ec73e9a3bdd1e7d4d40440fe91e76b63a
chaeha617 <91950670+chaeha617@users.noreply.github.com>
2023-06-29 18:12:29 +0900

    Update Main.java

--- 채하/untitled/src/Main.java
+++ 채하/untitled/src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
     	Scanner sc = new Scanner(System.in);
-    	Game g = new Game();
+    	Game game = new Game();
     	while(true) {
     		
     		g.startGame();
