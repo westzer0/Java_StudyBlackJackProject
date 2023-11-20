0ac9f02b3c93eaf079a469d9da6cc3b12dadb011
chaeha617 <91950670+chaeha617@users.noreply.github.com>
2023-11-20 14:22:55 +0900

    Delete 동복 directory

--- 동복/blackjack/src/main/java/org/example/Gamer.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public interface Gamer {
-
-    void hit(Deck deck);
-    void showCards();
-    int getScore();
-
-}
--- 동복/blackjack/src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        BlackjackGame blackjackGame = new BlackjackGame();
-        Player player = new Player();
-        blackjackGame.runGame(player);
-    }
-}
