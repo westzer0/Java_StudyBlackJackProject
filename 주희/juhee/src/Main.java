95b8cbc0589c6857d93fadf834efd62f2b4b1911
chaeha617 <91950670+chaeha617@users.noreply.github.com>
2023-11-20 14:23:12 +0900

    Delete 주희 directory

--- 주희/juhee/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("--------Jack's BlackJack Game--------");
-        BlackJackGame game = new BlackJackGame();
-        game.startGame();
-    }
-}
